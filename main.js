38c1f2b09633b37d40df6fae1d0cd09853d9814a
598459 <129184360+598459@users.noreply.github.com>
2024-09-10 10:48:06 -0300

    gff

--- main.js
+++ main.js
@@ -1,5 +1,54 @@
-const elemento = document.querySelector(".teste");
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPergunta = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
 
-const titulo = document.querySelector(".teste2");
 
-titulo.textContent = "Olá mundo";
+const perguntas = [
+    {
+        enunciado: "Qual a data da independênica do Brasil?",
+        alternativas: ["07 de Setembro","15 de Novembro","21 de Abril"]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: ["aternativa1","alternativa2","alternativa3"]
+    },
+    {
+        enunciado: "Pergunta 3",
+        alternativas: ["aternativa1","alternativa2","alternativa3"]
+    },
+    {
+        enunciado: "Pergunta 4",
+        alternativas: ["aternativa1","alternativa2","alternativa3"]
+    },
+
+
+];
+
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta(){
+perguntaAtual =  perguntas[atual];
+caixaPergunta.textContent = perguntaAtual.enunciado;
+
+}
+
+mostraPergunta();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
